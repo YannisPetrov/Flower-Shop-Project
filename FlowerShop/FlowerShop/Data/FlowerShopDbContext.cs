15b26ba58f40a15f8492f38c190e23f477c75207
Thireftis <yannis.petrov@gmail.com>
2022-08-08 15:40:12 +0300

    Identity Schema Added

---
+++ FlowerShop/FlowerShop/Data/FlowerShopDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowerShop.Data
+{
+    public class FlowerShopDbContext : IdentityDbContext
+    {
+        public FlowerShopDbContext(DbContextOptions<FlowerShopDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
